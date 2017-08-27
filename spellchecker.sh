3ce76616da2e1c7f48b894f0f2d83c3aeaae673f
Andrea Della Corte <andrea@dellacorte.me>
2017-08-27 17:35:43 +0100

    Additional tools & fixes

--- spellchecker.sh
+++ spellchecker.sh
@@ -1,17 +1,38 @@
 #!/bin/zsh
 
-read "?Enter Docs id" docId
+read "?Enter Docs Id from Google Drive: " docId
 
-mkdir -p /tmp/$docId
+[ "$docId" ] || (echo "Id is empty, exiting" && exit)
 
-cd /tmp/$docId
+mkdir -p ./tmp/$docId
+
+cd ./tmp/$docId
 
 filename=`gdrive info $docId | grep "Name: " | cut -d' ' -f2-`
 
-gdrive export $docId
+gdrive export --force $docId
 
 pdftotext "$filename.pdf"
 
-sed 's/\xe2\x80\x8b//g' "$filename.txt" | tr '\r\n' ' ' > ready_file.txt
+sed 's/\xe2\x80\x8b//g' "$filename.txt" | tr '\r\n' ' ' | tr -s ' ' > ready_file.txt
 
-languagetool ready_file.txt
+rm -f ../../findings.txt
+touch ../../findings.txt
+
+echo "# write-good" >> ../../findings.txt
+write-good ready_file.txt >> ../../findings.txt
+
+echo "-----------------------------------------------------\n" >> ../../findings.txt
+
+echo "# gender" >> ../../findings.txt
+egrep --color -n -i ".{0,10}( his | her | she | he ).{0,10}" ready_file.txt >> ../../findings.txt
+
+echo "-----------------------------------------------------\n" >> ../../findings.txt
+
+echo "# proselint" >> ../../findings.txt
+proselint ready_file.txt >> ../../findings.txt
+
+echo "-----------------------------------------------------\n" >> ../../findings.txt
+
+echo "# languagetool" >> ../../findings.txt
+languagetool ready_file.txt >> ../../findings.txt
