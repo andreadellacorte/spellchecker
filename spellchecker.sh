8e5bc56022df1de90ba9c2613e6b3f0fe7ef4ed3
Andrea <andreadellacorte85@gmail.com>
2017-08-27 15:49:20 +0100

    Create spellchecker.sh

---
+++ spellchecker.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+read -p "Enter Docs id" docId
+
+mkdir /tmp/$docId
+
+/tmp/$docId
+
+filename=`gdrive info $docId | grep "Name: " | cut -d' ' -f2-`
+
+gdrive export $docId
+
+pdftotext "$filename.pdf"
+
+sed 's/\xe2\x80\x8b//g' "$filename.txt" | tr '\r\n' ' ' > ready_file.txt
+
+languagetool ready_file.txt
